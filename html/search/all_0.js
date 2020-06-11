e660b7f46115ec5ab4e6306116cb72dd978aa250
Doxygen CI <travis@travis-ci.org>
2020-06-11 21:06:08 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['adafruit_5fzerofft_2eh',['Adafruit_ZeroFFT.h',['../_adafruit___zero_f_f_t_8h.html',1,'']]],
   ['align4',['ALIGN4',['../_adafruit___zero_f_f_t_8h.html#a280a402ab28c399fcc4168f2ed631acb',1,'Adafruit_ZeroFFT.h']]],
-  ['armbitrevtable',['armBitRevTable',['../group___c_f_f_t___c_i_f_f_t.html#gae247e83ad50d474107254e25b36ad42b',1,'armBitRevTable():&#160;arm_common_tables.c'],['../group___c_f_f_t___c_i_f_f_t.html#gae247e83ad50d474107254e25b36ad42b',1,'armBitRevTable():&#160;arm_common_tables.c']]],
-  ['adafruit_20zerofft_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fzerofft_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fzerofft_29',['Adafruit ZeroFFT Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_ZeroFFT.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_ZeroFFT)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['armbitrevtable',['armBitRevTable',['../group___c_f_f_t___c_i_f_f_t.html#gae247e83ad50d474107254e25b36ad42b',1,'armBitRevTable():&#160;arm_common_tables.c'],['../group___c_f_f_t___c_i_f_f_t.html#gae247e83ad50d474107254e25b36ad42b',1,'armBitRevTable():&#160;arm_common_tables.c']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -5,8 +5,7 @@ var indexSectionsWithContent =
   2: "z",
   3: "atw",
   4: "afqz",
-  5: "c",
-  6: "a"
+  5: "c"
 };
 
 var indexSectionNames =
@@ -16,8 +15,7 @@ var indexSectionNames =
   2: "functions",
   3: "variables",
   4: "defines",
-  5: "groups",
-  6: "pages"
+  5: "groups"
 };
 
 var indexSectionLabels =
@@ -27,7 +25,6 @@ var indexSectionLabels =
   2: "Functions",
   3: "Variables",
   4: "Macros",
-  5: "Modules",
-  6: "Pages"
+  5: "Modules"
 };
 
